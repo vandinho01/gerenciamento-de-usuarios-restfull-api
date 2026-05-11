ac84040d44259acbc1b9d704a527a1678c33629a
Vandinho <evandro.menshaid@gmail.com>
2026-05-11 12:48:51 -0300

    U06 - Consumindo Rotas, POST, PUT e DELETE com Restify

--- routes/users.js
+++ routes/users.js
@@ -17,9 +17,55 @@ router.get('/', function (req, res, next) {
 
     assert.ifError(err);
 
+    res.json(obj);
+
+  });
+});
+
+router.get('/:id', function (req, res, next) {
+
+  client.get(`/users/${req.params.id}`, function (err, request, response, obj) {
+
+    assert.ifError(err);
+
     res.end(JSON.stringify(obj, null, 2));
 
+  });
 });
+
+router.put('/:id', function (req, res, next) {
+
+  client.put(`/users/${req.params.id}`, req.body, function (err, request, response, obj) {
+
+    assert.ifError(err);
+
+    res.end(JSON.stringify(obj, null, 2));
+
+  });
+
+});
+
+router.delete('/:id', function (req, res, next) {
+
+  client.del(`/users/${req.params.id}`, function (err, request, response, obj) {
+
+    assert.ifError(err);
+
+    res.end(JSON.stringify(obj, null, 2));
+
+  });
+
+});
+
+router.post('/', function (req, res, next) {
+
+  client.post(`/users`, req.body, function (err, request, response, obj) {
+
+    assert.ifError(err);
+
+    res.end(JSON.stringify(obj, null, 2));
+
+  });
 
 });
 
