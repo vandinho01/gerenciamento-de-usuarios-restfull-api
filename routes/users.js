b8bb1600af086f84819d6dfb75f71e239ce0127d
Vandinho <evandro.menshaid@gmail.com>
2026-05-10 23:33:47 -0300

    U02 - Usando Restify para acessar API REST

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,26 @@
 var express = require('express');
+var assert = require('assert');
+var restify = require('restify-clients');
 var router = express.Router();
 
+// Creates a JSON client
+var client = restify.createJsonClient({
+
+  url: 'https://localhost:4000'
+
+});
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+
+  client.get('/users', function (err, request, response, obj) {
+
+    assert.ifError(err);
+
+    res.end(JSON.stringify(obj, null, 2));
+
+});
+
 });
 
 module.exports = router;
